e9c44f7f38fbabbff42db980f09bc8a31c29a55d
fjaviermp <fjaviermpinf@gmail.com>
2023-03-16 17:10:36 +0100

    Pagina inicial comenzada en plano

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,20 +1,124 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pruebas</title>
+    <script src="jquery-3.6.4.min.js"></script>
+    <script src="https://kit.fontawesome.com/1d66d8f1c5.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+        <div class="container">
+            
+          <a class="navbar-brand" href="#">Navbar</a>
+          
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+        </div>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        </div>
+      </nav>
 
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <section class="" id="landscape-image">
+        <div alt="Portada" class="landscape-file img-fluid"></div>
+
+        <div class="image-text">
+            <h1>Bienvenido a mi sitio web</h1>
+            <p>Este es un sitio web impresionante que he creado con HTML, CSS y Bootstrap.</p>
+            <a href="#" class="btn btn-primary btn-lg">¡Comencemos!</a>
+        </div>
+    </section>
+
+    <!-- Características -->
+    <section class="features-section">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="feature col-md-3">
+                    <div class="feature-item text-center">
+                        <div class="feature-icon">
+                            <i class="fas fa-globe"></i>
+                        </div>
+                        <h3>Global Reach</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet, erat in auctor efficitur,
+                        tortor ipsum aliquam purus.</p>
+                    </div>
+                </div>
+                <div class="feature col-md-3">
+                    <div class="feature-item text-center">
+                        <div class="feature-icon">
+                        <i class="fas fa-clock"></i>
+                        </div>
+                        <h3>24/7 Support</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet, erat in auctor efficitur,
+                        tortor ipsum aliquam purus.</p>
+                    </div>
+                </div>
+                <div class="feature col-md-3">
+                    <div class="feature-item text-center">
+                        <div class="feature-icon">
+                            <i class="fas fa-heart"></i>
+                        </div>
+                        <h3>Customer Love</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet, erat in auctor efficitur,
+                        tortor ipsum aliquam purus.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- About -->
+    <section class="about-section mt-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <img src="https://via.placeholder.com/600x400" alt="About Image" class="img-fluid">
+                </div>
+                <div class="col-md-6">
+                    <div class="about-content">
+                        <h2>About Us</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet, erat in auctor efficitur,
+                        tortor ipsum aliquam purus, vel ullamcorper urna lectus sit amet neque. Aliquam dictum elit est, eget
+                        bibendum tortor venenatis ac. Nunc sit amet mi nibh. Aliquam blandit vestibulum leo eget vehicula.
+                        Donec ut semper tortor, vel malesuada odio. Suspendisse quis ante eros. Curabitur eget metus at mi
+                        rhoncus malesuada non eget ex. Duis auctor felis quis nibh placerat posuere.</p>
+                        <p>Etiam eu malesuada dolor, eu sollicitudin leo. Vestibulum ante ipsum primis in faucibus orci luctus et
+                        ultrices posuere cubilia Curae; Duis malesuada laoreet quam. Integer sagittis nibh sed massa faucibus,
+                        at fringilla arcu pharetra. Nam posuere justo vel risus accumsan euismod. Maecenas in turpis urna.
+                        Praesent et urna velit. Sed pellentesque tincidunt orci, vitae rhoncus dolor volutpat a. Etiam eget
+                        mi orci. </p>
+                        <button class="btn btn-primary">Learn More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="footer-section mt-4">
+        <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            <p>&copy; 2023 Home Page. All rights reserved.</p>
+            </div>
+        </div>
+        </div>
+    </footer>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </html>
