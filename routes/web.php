5e19565d6b917d35544bb85f20e43c4a8b555bf9
fjaviermp <fjaviermpinf@gmail.com>
2023-05-31 11:43:48 +0200

    Arreglo url redireccion login

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,8 @@ Route::get('/contacto', function () {
 
 Route::get('/getOptions','App\Http\Controllers\OptionsController@getOptions');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/admin', function () {
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
+    Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });
