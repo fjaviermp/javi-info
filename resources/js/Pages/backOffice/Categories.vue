0dabf0e8884997b31cba781aaece6145ff05492a
fjaviermp <fjaviermpinf@gmail.com>
2023-06-08 09:06:09 +0200

    Mensaje mas claro

--- resources/js/Pages/backOffice/Categories.vue
+++ resources/js/Pages/backOffice/Categories.vue
@@ -17,7 +17,7 @@
                     </div>
 
                     <div class="flex justify-end">
-                        <button @click="deleteCategory()" type="submit" class="inline-flex items-center px-5 py-3 bg-green-400 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">Guardar</button>
+                        <button @click="deleteCategory()" type="submit" class="inline-flex items-center px-5 py-3 bg-green-400 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">Confirmar y eliminar</button>
                     </div>
                 </div>
             </div>
